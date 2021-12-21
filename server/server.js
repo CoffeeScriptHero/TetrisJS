e733cfd1e05ef0e741556de947efcbf491a71adc
CoffeeScriptHero <denis_kozarenko@ukr.net>
2021-12-21 14:50:58 +0200

    structure changed

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
+app.use(express.static(__dirname + "../../public"));
 app.use("/", routes);
 app.use("/", secureRoutes);
 
